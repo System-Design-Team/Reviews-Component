ad0736fc9770eb856afa4eab4d8588f6f5a87414
Kody Maus <auto.mausx@gmail.com>
2020-04-30 16:38:47 -0600

    updated and populated DB schema, wrote routes for service as well as query strings for mysql

---
+++ server/queries.js
@@ -0,0 +1,38 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'admin',
+  database: 'worstBuyReviews'
+})
+
+connection.connect();
+
+// gets all reviews
+
+const getAllreviews = (callback) => {
+  connection.query('SELECT * FROM reviews' (err, data) => {
+    if (err) {
+      callback(err, null)
+    } else {
+      callback(null, data)
+    }
+  })
+};
+
+// gets reviews from DB where sku matches one sent in get req
+
+const getReviewsForProduct = (sku, callback) => {
+  connection.query('SELECT * FROM reviews WHERE sku = (?)', [sku], (err, data) => {
+    if (err) {
+      callback(err, null)
+    } else {
+      callback(null, err)
+    }
+  })
+};
+
+module.exports = {
+  getAllreviews,
+  getReviewsForProduct
+}
