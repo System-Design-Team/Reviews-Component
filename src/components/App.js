5a8542288bb4aa5b8bca5c0ca0f69e4510cdfec2
Kody Maus <auto.mausx@gmail.com>
2020-05-01 13:51:00 -0600

    set up axios get request to populate front end with reviews

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,48 @@
 import React, { Component } from "react";
+import axios from 'axios';
 
 export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      message: "Hello World"
+      reviews: []
     };
+    this.getAllReviews = this.getAllReviews.bind(this);
   }
+
+  componentDidMount() {
+    this.getAllReviews();
+  }
+
+  getAllReviews() {
+    axios.get('http://127.0.0.1:9004/api/reviews')
+    .then(res => {
+      const reviews = res.data;
+      this.setState({reviews})
+    })
+    .catch(err => {
+      if (err) {
+        console.log(err);
+        console.log(err.message);
+      }
+    })
+  }
+
   render() {
     return (
       <div>
-        <p>{this.state.message}</p>
+        {this.state.reviews.map((review) => {
+          if (review.product_sku === 510121) {
+            return (
+              <div>
+              <p>{review.username}</p>
+              <h4><p>{review.review_title}</p></h4>
+            <p>{review.review_body}</p>
+            <p>{review.date}</p>
+            </div>
+            )
+          }
+        })}
       </div>
     );
   }
