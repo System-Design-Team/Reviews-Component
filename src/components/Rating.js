b42d799b91c1a266a27472b6b54cbc865f16e91f
Kody Maus <auto.mausx@gmail.com>
2020-05-05 15:03:03 -0600

    mostly complete styling for review list

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -12,25 +12,17 @@ class Rating extends React.Component {
       review: this.props.reviews
     }
     this.averageStars = this.averageStars.bind(this);
-    // this.iterateReviews = this.iterateReviews.bind(this);
   }
 
-  // iterateReviews(e) {
-  //   e.preventDefault()
-  //   this.setState({review: this.props.reviews})
-  //   this.averageStars(this.props.reviews, this.props.sku)
-  // }
 
 
   averageStars(reviews, sku) {
     let skuArray = [];
-    //console.log(reviews)
     reviews.map((item) => {
       if (item.product_sku === sku) {
         skuArray.push(item.star_rating)
       }
     })
-    console.log(skuArray)
     let total = skuArray.reduce((a, b) => a + b);
     let results = total / skuArray.length;
      this.setState({ averageStars: results});
@@ -39,28 +31,16 @@ class Rating extends React.Component {
 
   componentDidUpdate(prevProps) {
     if (this.props.reviews !== prevProps.reviews) {
-      console.log(this.props)
-      console.log(prevProps)
-      console.log(this.props.reviews)
       this.averageStars(this.props.reviews, this.state.sku)
-      //this.setState({review: this.props.reviews}, console.log(this.state.review))
     }
   }
-
-  // getDerivedStateFromProps() {
-  //   console.log(this.props.reviews)
-  //   //this.averageStars(this.props.reviews, this.props.sku)
-  //   this.setState({review: this.props.reviews})
-  // }
 
 
 
   render() {
-    // {console.log(this.state.review)}
     return(
       <div>
-            <StarRatings rating={this.state.averageStars} starRatedColor='yellow' numberOfStars={5} />
-            {/* <button onClick={this.iterateReviews}>click</button> */}
+            <StarRatings rating={this.state.averageStars} starRatedColor='yellow' numberOfStars={5} starSpacing='0px'/>
       </div>
     )
   }
